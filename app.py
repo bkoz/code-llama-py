aa3f1c3ca0bea30ac038e6c5f95c334a10fbd8b1
Bob Kozdemba <bkozdemba@gmail.com>
2023-08-29 15:43:08 -0500

    updated

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ client = Client(
 
 
 def generate(
-    prompt, temperature=0.9, max_new_tokens=256, max_length=500, top_p=0.95, repetition_penalty=1.0,
+    prompt, temperature=0.9, max_new_tokens=256, top_p=0.95, repetition_penalty=1.0,
 ):
 
     temperature = float(temperature)
@@ -58,7 +58,8 @@ def generate(
         top_p=top_p,
         repetition_penalty=repetition_penalty,
         do_sample=True,
-        seed=42
+        seed=42,
+        max_length=500
     )
 
     if FIM_INDICATOR in prompt:
